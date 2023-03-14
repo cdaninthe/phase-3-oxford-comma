bdcb75371117f0b1cb6763ad7350f97a1a3f28b0
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-03-14 18:57:19 -0400

    commit tree c71ae3f1235432a9f1d091bafe78a80d9d26982d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.length == 1 
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+        last_word = array.pop
+        array << "and " + last_word
+        array.join(", ")
+    else
+        last_word = array.pop
+        array << "and " + last_word
+        array.join(", ")
+    end
 end
